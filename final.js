9e45d6031f295b00ae7411bde75e910c9384003a
Ayan Sujon <ayansujonbd@gmail.com>
2025-08-04 00:06:36 +0600

    Problem 2 resolved

--- final.js
+++ final.js
@@ -8,4 +8,19 @@ var divideBy = 2;
 
 // ===================================================
 
+// Problem -02 ( Cycle or Laptop ) 
+var money = 9999;
+//write your code here
+
+
+if( money >= 25000){
+    console.log('Laptop');
+}
+else if(money >= 10000){
+    console.log('Cycle');
+}
+else{
+    console.log('Chocolate');
+}
+   
 
--- problem2.js
+++ problem2.js
@@ -23,3 +23,19 @@ var money = 10000;
 
 
 */
+
+// Problem -02 ( Cycle or Laptop ) 
+var money = 9999;
+//write your code here
+
+
+if( money >= 25000){
+    console.log('Laptop');
+}
+else if(money >= 10000){
+    console.log('Cycle');
+}
+else{
+    console.log('Chocolate');
+}
+   
